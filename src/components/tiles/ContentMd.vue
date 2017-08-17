7acbbf8e6f3d8a28e7dca67dabe6ec2163118530
Collier Devlin <collier.devlin@leancor.com>
2017-08-17 15:43:30 -0400

    Initial commit

---
+++ src/components/tiles/ContentMd.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :id="id" class="item"
+    :style="{
+      'width': '100%',
+      'max-width': width,
+      'height': height
+    }">
+    <div class="item-content link-tile">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'content-md',
+    props: {
+      id: {
+        type: [Number, String],
+        default: 'tile'
+      },
+      width: {
+        type: String,
+        default: '410px'
+      },
+      height: {
+        type: String,
+        default: '410px'
+      },
+      onClick: {
+        type: Function,
+        default (id) {
+          return null
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
